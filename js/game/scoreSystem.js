47d21f4871ead3677a80fac5fb273a8dacc8b163
Marmiril <angel.plata@outlook.es>
2026-08-18 20:03:42 +0200

    ScoreSystem

---
+++ js/game/scoreSystem.js
@@ -0,0 +1,53 @@
+export class ScoreSystem {
+    constructor(maxPoints = 5) {
+        this.maxPoints = maxPoints;
+        
+        this.playerPoints = playerPoints;3
+        this.cpuPoints = cpuPoints;
+
+        this.matchEnded = false;
+        this.winner = null;
+    }  
+
+    pointPlayer() {
+        if (this.matchEnded) {
+            return;
+        }
+        
+        this.playerPoints++;
+
+        if (this.playerPoints >= this.maxPoints) {
+            this.matchEnded = true;
+            this.winner = "PLAYER";
+        }
+    }
+
+    pointCpu() {
+        if (this.matchEnded) {
+            return;
+        }
+
+        this.cpuPoints++;
+
+        if (this.cpuPoints >= this.maxPoints) {
+            this.matchEnded = true;
+            this.winner = "CPU";
+        }
+    }
+
+    resetMatch() {
+        this.playerPoints = 0;
+        this.cpuPoints = 0;
+
+        this.matchEnded = false;
+        this.winner = null;
+    }
+
+    getPlayerPoints() { return this.playerPoints; }
+    getCpuPoints() { return this.cpuPoints; }
+    isMatchEnded() { return this.matchEnded; }
+
+    
+     
+ 
+}
